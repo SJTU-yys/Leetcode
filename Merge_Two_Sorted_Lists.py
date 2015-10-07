db6417d066644d00d2d3e198509180fbb4c3a1e6
dubur <yaoys12345@gmail.com>
2015-10-07 22:16:48 +0800

    Merge two sorted lists

---
+++ Merge_Two_Sorted_Lists.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  7 22:14:25 2015
+
+@author: yys
+"""
+
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+            
+        dummy = tail = ListNode(-1)
+        while l1 or l2:
+            if l1 and l2:
+                if l1.val < l2.val:
+                    tail.next = l1
+                    l1 = l1.next
+                else:
+                    tail.next = l2
+                    l2=l2.next
+            elif l1:
+                tail.next = l1
+                l1 = l1.next
+            else:
+                tail.next = l2
+                l2 = l2.next
+            tail = tail.next
+        
+        return dummy.next
